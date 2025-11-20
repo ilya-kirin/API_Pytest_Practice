2b156b57145d9b19789bf7a0a36c7d292a38519a
ilya-kirin <kirinia434@gmail.com>
2025-11-20 22:29:27 +0400

    day 3: config files are moved to relevant package, fixture parameterization added

--- conftest.py
+++ conftest.py
@@ -6,6 +6,12 @@ def global_setup_teardown():
     yield
     print("Executing teardown from 'global_setup_teardown' fixture from conftest file")
 
+@pytest.fixture(scope='class')
+def class_setup_teardown():
+    print("Executing setup from 'class_setup_teardown' fixture from conftest file")
+    yield
+    print("Executing teardown from 'class_setup_teardown' fixture from conftest file")
+
 @pytest.fixture()
 def name_shadowing_fixture():
     print("Executing setup from 'name_shadowing_fixture' fixture from conftest file")
